f2f9212954b4f9f819729ffaca3ddf7047147c3b
Frederik Rothenberger <frothenb@student.ethz.ch>
2015-10-28 14:27:23 +0100

    More formatting and cleanup.

--- source/Program.cs
+++ source/Program.cs
@@ -12,51 +12,52 @@ namespace Z3AxiomProfiler
     {
         static void parseErrorCommandLineArguments(string err)
         {
-          System.Console.WriteLine("Aborting parsing command line arguments:\n" + err);
-          printUsage();
-          System.Environment.Exit(1);
+            Console.WriteLine("Aborting parsing command line arguments:\n" + err);
+            printUsage();
+            Environment.Exit(1);
         }
 
         static void printUsage()
         {
-          System.Console.WriteLine("Usage: Z3AxiomProfiler [options] <prelude-file> <filename>");
-          System.Console.WriteLine("       prelude-file       : VCC prelude file location");
-          System.Console.WriteLine("       filename           : Boogie input file");
-          System.Console.WriteLine("       options ");
-          System.Console.WriteLine("          /f:<function>   : Function name");
-          System.Console.WriteLine("          /t:<seconds>    : Verification timeout");
-          System.Console.WriteLine("          /l:<file>       : Log file to process");
-          System.Console.WriteLine("          /s              : Skip conflicts/decisions (conserves memory)");
-          System.Console.WriteLine("          /v1             : Start Z3 v1 (default)");
-          System.Console.WriteLine("          /v2             : Start Z3 v2");
-          System.Environment.Exit(0);
+            Console.WriteLine("Usage: Z3AxiomProfiler [options] <prelude-file> <filename>");
+            Console.WriteLine("       prelude-file       : VCC prelude file location");
+            Console.WriteLine("       filename           : Boogie input file");
+            Console.WriteLine("       options ");
+            Console.WriteLine("          /f:<function>   : Function name");
+            Console.WriteLine("          /t:<seconds>    : Verification timeout");
+            Console.WriteLine("          /l:<file>       : Log file to process");
+            Console.WriteLine("          /s              : Skip conflicts/decisions (conserves memory)");
+            Console.WriteLine("          /v1             : Start Z3 v1 (default)");
+            Console.WriteLine("          /v2             : Start Z3 v2");
+            Environment.Exit(0);
         }
 
-        private static void InitConfig(Z3AxiomProfiler z3vis, string[] args) {
+        private static void InitConfig(Z3AxiomProfiler z3vis, string[] args)
+        {
             string error_msg;
-            if ( !z3vis.parseCommandLineArguments(args, out error_msg))
+            if (!z3vis.parseCommandLineArguments(args, out error_msg))
             {
                 parseErrorCommandLineArguments(error_msg);
             }
         }
-      
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
-          //AllocConsole();
-          //Console.OpenStandardOutput();
-          //Console.OpenStandardError();
-          //AttachConsole(ATTACH_PARENT_PROCESS);
+            //AllocConsole();
+            //Console.OpenStandardOutput();
+            //Console.OpenStandardError();
+            //AttachConsole(ATTACH_PARENT_PROCESS);
 
-          Application.EnableVisualStyles();
-          Application.SetCompatibleTextRenderingDefault(false);
-          Z3AxiomProfiler f = new Z3AxiomProfiler();
-          InitConfig(f,args);
-          f.Show();
-          Application.Run(f);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Z3AxiomProfiler f = new Z3AxiomProfiler();
+            InitConfig(f, args);
+            f.Show();
+            Application.Run(f);
         }
 
     }
