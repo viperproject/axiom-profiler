ef901e9d401d2b34d041684ac309b8e12a64afbd
Nils Becker <nbecker@student.ethz.ch>
2018-05-04 08:47:17 +0200

    adding files

---
+++ source/PrettyPrinting/EqualityExplanationPrinter.cs
@@ -0,0 +1,142 @@
+﻿using AxiomProfiler.QuantifierModel;
+using System;
+using System.Linq;
+
+namespace AxiomProfiler.PrettyPrinting
+{
+
+    class EqualityExplanationPrinter : EqualityExplanationVisitor<object, Tuple<InfoPanelContent, PrettyPrintFormat, bool, int>>
+    {
+        public static readonly EqualityExplanationPrinter singleton = new EqualityExplanationPrinter();
+
+        private static string getIndentString(PrettyPrintFormat format, int descriptionIndent)
+        {
+            var descriptionIndentString = PrintConstants.indentDiff + String.Join("", Enumerable.Repeat(" ", descriptionIndent - 2));
+            var explanationIndentString = String.Join("", Enumerable.Repeat(PrintConstants.indentDiff, format.CurrentEqualityExplanationPrintingDepth));
+            return descriptionIndentString + explanationIndentString;
+        }
+
+        public override object Direct(DirectEqualityExplanation target, Tuple<InfoPanelContent, PrettyPrintFormat, bool, int> arg)
+        {
+            var content = arg.Item1;
+            var format = arg.Item2;
+            var shouldPrintSource = arg.Item3;
+            var indent = arg.Item4;
+
+            var indentString = getIndentString(format, indent);
+
+            if (shouldPrintSource)
+            {
+                content.Append(indentString);
+                target.source.PrettyPrint(content, format, indent);
+            }
+            content.switchToDefaultFormat();
+            content.Append($"\n{indentString}");
+            target.equality.printName(content, format);
+            if (target.equality.Responsible != null)
+            {
+                content.Append($" ({target.equality.Responsible.Quant.PrintName})");
+            }
+            else
+            {
+                content.Append($" (asserted)");
+            }
+            content.Append($"\n{indentString}");
+            target.target.PrettyPrint(content, format, indent);
+
+            return null;
+        }
+
+        public override object Transitive(TransitiveEqualityExplanation target, Tuple<InfoPanelContent, PrettyPrintFormat, bool, int> arg)
+        {
+            var content = arg.Item1;
+            var format = arg.Item2;
+            var shouldPrintSource = arg.Item3;
+            var indent = arg.Item4;
+
+            var indentString = getIndentString(format, indent);
+
+            for (var i = 0; i < target.equalities.Length; ++i)
+            {
+                visit(target.equalities[i], Tuple.Create(content, format, i == 0 && shouldPrintSource, indent));
+            }
+
+            if (target.equalities.Length == 0)
+            {
+                if (shouldPrintSource)
+                {
+                    content.Append(indentString);
+                    target.source.PrettyPrint(content, format, indent);
+                }
+                content.switchToDefaultFormat();
+                content.Append($"\n{indentString}=\n{indentString}");
+                target.target.PrettyPrint(content, format, indent);
+            }
+
+            return null;
+        }
+
+        public override object Congruence(CongruenceExplanation target, Tuple<InfoPanelContent, PrettyPrintFormat, bool, int> arg)
+        {
+            var content = arg.Item1;
+            var format = arg.Item2;
+            var shouldPrintSource = arg.Item3;
+            var indent = arg.Item4;
+
+            var indentString = getIndentString(format, indent);
+
+            if (shouldPrintSource)
+            {
+                content.Append(indentString);
+                target.source.PrettyPrint(content, format, indent);
+            }
+
+            content.switchToDefaultFormat();
+
+            content.Append($"\n{indentString}= (congruence)\n");
+
+            if (format.CurrentEqualityExplanationPrintingDepth < format.MaxEqualityExplanationPrintingDepth)
+            {
+                content.Append($"{indentString}\n{indentString}{PrintConstants.indentDiff}Argument Equalities:\n");
+                var recursiveArg = Tuple.Create(content, format.NextEqualityExplanationPrintingDepth(), true, indent);
+
+                visit(target.sourceArgumentEqualities.First(), recursiveArg);
+                foreach (var eqality in target.sourceArgumentEqualities.Skip(1))
+                {
+                    content.switchToDefaultFormat();
+                    content.Append($"\n{indentString}{PrintConstants.indentDiff}\n");
+                    visit(eqality, recursiveArg);
+                }
+                content.switchToDefaultFormat();
+                content.Append($"\n{indentString}\n");
+            }
+
+            content.Append(indentString);
+            target.target.PrettyPrint(content, format, indent);
+
+            return null;
+        }
+
+        public override object RecursiveReference(RecursiveReferenceEqualityExplanation target, Tuple<InfoPanelContent, PrettyPrintFormat, bool, int> arg)
+        {
+            var content = arg.Item1;
+            var format = arg.Item2;
+            var shouldPrintSource = arg.Item3;
+            var indent = arg.Item4;
+
+            var indentString = getIndentString(format, indent);
+
+            if (shouldPrintSource)
+            {
+                content.Append(indentString);
+                target.source.PrettyPrint(content, format, indent);
+            }
+            content.switchToDefaultFormat();
+            content.Append($"\n{indentString}= ({target.EqualityNumber})-{target.GenerationOffset}");
+            content.Append($"\n{indentString}");
+            target.target.PrettyPrint(content, format, indent);
+
+            return null;
+        }
+    }
+}
---
+++ source/QuantifierModel/EqualityExplanation.cs
@@ -0,0 +1,270 @@
+﻿using AxiomProfiler.PrettyPrinting;
+using System;
+
+namespace AxiomProfiler.QuantifierModel
+{
+    public abstract class EqualityExplanation
+    {
+        public readonly Term source, target;
+
+        protected EqualityExplanation(Term source, Term target)
+        {
+            this.source = source;
+            this.target = target;
+        }
+
+        abstract internal protected R Accept<R, A>(EqualityExplanationVisitor<R, A> visitor, A arg);
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var other = (EqualityExplanation) obj;
+            return source.id == other.source.id && target.id == other.target.id;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return source.GetHashCode();
+        }
+
+        public void PrettyPrint(InfoPanelContent content, PrettyPrintFormat format, int eqNumber)
+        {
+            var numberingString = $"({eqNumber}) ";
+            content.switchToDefaultFormat();
+            content.Append(numberingString);
+            source.PrettyPrint(content, format, numberingString.Length);
+            EqualityExplanationPrinter.singleton.visit(this, Tuple.Create(content, format, false, numberingString.Length));
+        }
+    }
+
+    public class DirectEqualityExplanation : EqualityExplanation
+    {
+        public readonly Term equality;
+
+        public DirectEqualityExplanation(Term source, Term target, Term equality) : base(source, target)
+        {
+            this.equality = equality;
+        }
+        
+        override internal protected R Accept<R, A>(EqualityExplanationVisitor<R, A> visitor, A arg)
+        {
+            return visitor.Direct(this, arg);
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType() || !base.Equals(obj))
+            {
+                return false;
+            }
+
+            var other = (DirectEqualityExplanation) obj;
+            return equality.id == other.equality.id;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return source.GetHashCode();
+        }
+    }
+
+    public class TransitiveEqualityExplanation : EqualityExplanation
+    {
+        public readonly EqualityExplanation[] equalities;
+
+        public TransitiveEqualityExplanation(Term source, Term target, EqualityExplanation[] equalities) : base(source, target)
+        {
+            this.equalities = equalities;
+        }
+
+        override internal protected R Accept<R, A>(EqualityExplanationVisitor<R, A> visitor, A arg)
+        {
+            return visitor.Transitive(this, arg);
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (ReferenceEquals(this, obj)) return true;
+
+            if (obj == null || GetType() != obj.GetType() || !base.Equals(obj))
+            {
+                return false;
+            }
+
+            var other = (TransitiveEqualityExplanation) obj;
+            if (equalities.Length != other.equalities.Length) return false;
+            for (var i = 0; i < equalities.Length; ++i)
+            {
+                if (!equalities[i].Equals(other.equalities[i])) return false;
+            }
+            return true;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return source.GetHashCode();
+        }
+    }
+
+    public class CongruenceExplanation : EqualityExplanation
+    {
+        public readonly EqualityExplanation[] sourceArgumentEqualities;
+
+        public CongruenceExplanation(Term source, Term target, EqualityExplanation[] sourceArgumentEqualities) : base(source, target)
+        {
+            this.sourceArgumentEqualities = sourceArgumentEqualities;
+        }
+
+        override internal protected R Accept<R, A>(EqualityExplanationVisitor<R, A> visitor, A arg)
+        {
+            return visitor.Congruence(this, arg);
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (ReferenceEquals(this, obj)) return true;
+
+            if (obj == null || GetType() != obj.GetType() || !base.Equals(obj))
+            {
+                return false;
+            }
+
+            var other = (CongruenceExplanation) obj;
+            foreach (var equality in sourceArgumentEqualities)
+            {
+                var found = false;
+                foreach (var otherEquality in other.sourceArgumentEqualities)
+                {
+                    if (equality.Equals(otherEquality))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) return false;
+            }
+
+            foreach (var equality in other.sourceArgumentEqualities)
+            {
+                var found = false;
+                foreach (var otherEquality in sourceArgumentEqualities)
+                {
+                    if (equality.Equals(otherEquality))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) return false;
+            }
+
+            return true;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return source.GetHashCode();
+        }
+    }
+
+    public class RecursiveReferenceEqualityExplanation: EqualityExplanation
+    {
+        public readonly int EqualityNumber;
+        public readonly int GenerationOffset;
+
+        public RecursiveReferenceEqualityExplanation(Term source, Term target, int EqualityNumber, int GenerationOffset): base(source, target)
+        {
+            this.EqualityNumber = EqualityNumber;
+            this.GenerationOffset = GenerationOffset;
+        }
+
+        protected internal override R Accept<R, A>(EqualityExplanationVisitor<R, A> visitor, A arg)
+        {
+            return visitor.RecursiveReference(this, arg);
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var other = (RecursiveReferenceEqualityExplanation) obj;
+
+            if (EqualityNumber != other.EqualityNumber || GenerationOffset != other.GenerationOffset) return false;
+
+            return base.Equals(obj);
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return source.GetHashCode();
+        }
+    }
+
+    public abstract class EqualityExplanationVisitor<R, A>
+    {
+        public R visit(EqualityExplanation target, A arg)
+        {
+            return target.Accept(this, arg);
+        }
+
+        public abstract R Direct(DirectEqualityExplanation target, A arg);
+
+        public abstract R Transitive(TransitiveEqualityExplanation target, A arg);
+
+        public abstract R Congruence(CongruenceExplanation target, A arg);
+
+        public abstract R RecursiveReference(RecursiveReferenceEqualityExplanation target, A arg);
+    }
+}
