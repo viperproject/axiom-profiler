a59e56598fdcbdcabaf70a98cb7da3446c03cd87
Arshavir Ter-Gabrielyan <ter-gabrielyan@inf.ethz.ch>
2018-08-18 12:31:57 +0200

    Added a script that generates a single Z3 log file from a Viper program (via Silicon). Tested on MacOS.

---
+++ scripts/get_z3_qtrace.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+timestamp() {
+  date +"%Y-%m-%d_%H-%M-%S"
+}
+
+size() {
+  stat -f%z $1
+}
+
+VIPERFILE=$1
+#echo ">>>>> VIPERFILE==$VIPERFILE"
+
+FILENAME=$(basename $VIPERFILE)
+#echo ">>>>> FILENAME==$FILENAME"
+
+Z3LOGFILE="/tmp/$FILENAME--$(timestamp)"
+#echo ">>>>> Z3LOGFILE==$Z3LOGFILE"
+
+echo ">>>>> Writing Z3 logs with prefix $Z3LOGFILE ..."
+
+Z3_EXE=/usr/local/Viper/z3/bin/z3
+
+SILICON="java -cp :/usr/local/Viper/backends/* viper.silicon.SiliconRunner --numberOfParallelVerifiers=1 --z3Exe=$Z3_EXE --z3LogFile=$Z3LOGFILE $VIPERFILE"
+
+echo ">>>>> Running $SILICON"
+
+eval $SILICON
+
+IN_FILES=$(find /tmp/ -maxdepth 1 -name "$FILENAME*.smt2")
+
+#echo ">>>>> Looking for the biggest SMT2 trace among:\n $IN_FILES ..."
+
+BIGGEST_TRACE=
+
+for i in $IN_FILES; do
+    curr_size=$(size $i)
+    if [ $curr_size -gt $(size $BIGGEST_TRACE) ]
+    then
+        echo ">>>>> Found a bigger trace: $i (size==$curr_size)"
+        BIGGEST_TRACE=$i
+    fi;
+done
+
+#echo ">>>>> The size of the biggest trace is: $(size $BIGGEST_TRACE)"
+
+Z3="$Z3_EXE TRACE=true PROOF=true $BIGGEST_TRACE"
+
+echo ">>>>> Running $Z3"
+
+eval $Z3
+
+echo ">>>>> Done. You should find z3.log in your current directory."
