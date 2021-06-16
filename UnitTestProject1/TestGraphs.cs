ea50c76039279670745d04b5dc798bac08345d79
Tiger Wu <tigerthepro@gmail.com>
2021-06-16 09:33:21 -0700

    test files with correct framework

---
+++ UnitTestProject1/TestGraphs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Msagl.Drawing;
+using AxiomProfiler;
+using AxiomProfiler.QuantifierModel;
+
+namespace UnitTestProject1
+{
+    // A visulization and explination for the test graphs can be found here
+    // https://docs.google.com/document/d/1SJspfBecgkVT9U8xC-MvQ_NPDTGVfg0yqq7YjJ3ma2s/edit?usp=sharing
+    class TestGraphs
+    {
+        public List<Quantifier> Quants = new List<Quantifier>(); // Quantifiers used in unit tests
+        public BindingInfo Info; // BindingInfo used to make nodes
+        public Graph graph1;
+        public Node node1;
+        public TestGraphs()
+        {
+            InitQuantsAndInfo();
+            MakeGraph1();
+            node1 = new Node("a");
+        }
+
+        // initialize 9 quantifiers
+        private void InitQuantsAndInfo()
+        {
+            // initialize Quants
+            for (int i = 0; i < 9; i++)
+            {
+                Quants.Add(new Quantifier());
+                Quants[i].PrintName = i.ToString();
+            }
+            // initialize Info, with meaningless arguments
+            Term[] args = new Term[0];
+            Term Term = new Term("a", args);
+            Info = new BindingInfo(Quants[0], args, args);
+        }
+
+        // function that make of node with Id = nodeId,
+        // and UserDate = a instantiation with quantifier quant
+        private Node MakeNode(String nodeId, int quant)
+        {
+            Node node = new Node(nodeId);
+            Instantiation Inst = new Instantiation(Info, "a");
+            Inst.Quant = Quants[quant];
+            node.UserData = Inst;
+            return node;
+        }
+
+        //Graph one is graph containing only one node
+        private void MakeGraph1()
+        {
+            graph1 = new Graph();
+            graph1.AddNode(MakeNode("A", 0));
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using AxiomProfiler;
+using AxiomProfiler.QuantifierModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using Microsoft.Msagl.Core.Routing;
+using Microsoft.Msagl.GraphViewerGdi;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.Layout.Layered;
+using AxiomProfiler.QuantifierModel;
+
+
+namespace UnitTestProject1
+{
+    // This test file is created to test some of pathExplanationButton_Click's helper functions
+    // AllDownPatterns
+    [TestClass]
+    public class DAGViewTest
+    {
+        static TestGraphs Graphs = new TestGraphs();
+
+        // The following are tests for AllDownPatterns
+
+        [TestMethod]
+        public void TestAllDownPatterns1()
+        {
+            List<List<Quantifier>> result = DAGView.AllDownPattern(Graphs.graph1.FindNode("a"), 8);
+            Assert.AreEqual(0, result.Count);
+            //Assert.AreEqual("a", Graphs.node1.Id);
+        }
+    }
+}
